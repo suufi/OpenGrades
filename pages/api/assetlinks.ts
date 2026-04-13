bb3db7c7ee0295f90fe5b74e9d187dbaa2a82452
suufi <12759332+suufi@users.noreply.github.com>
2026-04-13 19:16:07 -0400

    Fix android assetlinks.ts

--- pages/api/assetlinks.ts
+++ pages/api/assetlinks.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json({
+    res.status(200).json([{
         relation: [
             "delegate_permission/common.handle_all_urls",
             "delegate_permission/common.get_login_creds"
@@ -11,10 +11,10 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
             namespace: "android_app",
             package_name: "edu.mit.OpenGrades",
             sha256_cert_fingerprints: [
-                "C8:1A:7E:3D:D7:5F:45:5D:19:FD:AB:2F:60:80:B2:46:B1:85:36:2E:EB:C1:54:A2:62:D5:9A:BB:5F:3B:D2:D8"
+                "C8:1A:7E:3D:D7:5F:45:5D:19:FD:AB:2F:60:80:B2:46:B1:85:36:2E:EB:C1:54:A2:62:D5:9A:BB:5F:3B:D2:D8",
             ]
         }
-    });
+    }]);
 }
 
 
